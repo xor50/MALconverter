076d15b0a95b91b1846d33f08f7027a9924c38ba
xor50 <37473184+xor50@users.noreply.github.com>
2021-08-02 01:45:18 +0200

    added calcALUValue

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,29 @@ public class Main {
 		String malCode = "MDR = TOS = MDR + H; wr; goto Main1";
 		System.out.println("input: " + malCode);
 
-		String malCodeNoSpaces = malCode.replaceAll("\\s+","");
+		String malCodeNoSpaces = malCode.replaceAll("\\s+", "");
 //		System.out.println(malCodeNoSpaces);
 
 		long numberOfCBusTargets = malCodeNoSpaces.chars().mapToObj(c -> (char) c).filter(x -> x.equals('=')).count();
 
-		String [] malPartsSplitEquals = malCodeNoSpaces.split("=");
+		String[] malPartsSplitEquals = malCodeNoSpaces.split("=");
 //		System.out.println(Arrays.toString(malPartsSplitEquals));
-		String [] malPartsSplitSemicolon = malPartsSplitEquals[malPartsSplitEquals.length-1].split(";");
+		String[] malPartsSplitSemicolon = malPartsSplitEquals[malPartsSplitEquals.length - 1].split(";");
 //		System.out.println(Arrays.toString(malPartsSplitSemicolon));
 
 		System.out.println(calcCBusValue(malPartsSplitEquals, (int) (numberOfCBusTargets)));
 
+		String malPartNextAddress = malPartsSplitSemicolon[malPartsSplitSemicolon.length - 1].substring(4); //take last array entry; remove goto (via substring)
+		System.out.println(calcNextAddress(malPartNextAddress));
+
+		if (malPartsSplitSemicolon.length == 3) {
+			System.out.println(calcMemActions(malPartsSplitSemicolon[malPartsSplitSemicolon.length - 2]));
+		}
+
+		System.out.println(calcALUValue(malPartsSplitSemicolon[0]));
+
 //		int [] microinstruction = new int[36];
 		long microinstructionDec = 0; // just start adding values for the bits as 2^n
-
-		String malPartNextAddress = malPartsSplitSemicolon[malPartsSplitSemicolon.length-1].substring(4); //take last array entry; remove goto (via substring)
-		System.out.println(calcNextAddress(malPartNextAddress));
 
 
 //		System.out.println(hexToBin("100"));
@@ -34,12 +40,75 @@ public class Main {
 //		System.out.println(binToHex(binaryString));
 	}
 
-	public static String binToHex(String binaryString){
-		return Integer.toHexString(Integer.parseInt(binaryString,2));
+	public static String binToHex(String binaryString) {
+		return Integer.toHexString(Integer.parseInt(binaryString, 2));
 	}
 
-	public static String hexToBin(String hexString){
-		return Integer.toBinaryString(Integer.parseInt(hexString,16));
+	public static String hexToBin(String hexString) {
+		return Integer.toBinaryString(Integer.parseInt(hexString, 16));
+	}
+
+	public static long calcBBusValue(String s) {
+		s.contains("MBR"); //todo
+		return 0;
+	}
+
+	public static long calcALUValue(String s) {
+
+		System.out.println(s);
+
+		final long INC = (long) Math.pow(2, 16);
+		final long INVA = (long) Math.pow(2, 17);
+		final long ENB = (long) Math.pow(2, 18);
+		final long ENA = (long) Math.pow(2, 19);
+		final long F1 = (long) Math.pow(2, 20);
+		final long F0 = (long) Math.pow(2, 21);
+
+		//A
+		if (s.matches("H"))
+			return F1 + ENA;
+		//B
+		if (s.matches("MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC"))
+			return F1 + ENB;
+		//B + A
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)\\+H"))
+			return F0 + F1 + ENA + ENB;
+		//B + A + 1
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)\\+H\\+1"))
+			return F0 + F1 + ENA + ENB + INC;
+		//A + 1
+		if (s.matches("H\\+1"))
+			return F0 + F1 + ENA + INC;
+		//B + 1
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)\\+1"))
+			return F0 + F1 + ENB + INC;
+		//B - 1
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)(-|−)1"))
+			return F0 + F1 + ENB + INVA;
+		//B - A
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)(-|−)H"))
+			return F0 + F1 + ENA + ENB + INVA + INC;
+		//-A
+		if (s.matches("(-|−)H"))
+			return F0 + F1 + ENA + INVA + INC;
+		//B AND A
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)ANDH"))
+			return ENA + ENB;
+		//B OR A
+		if (s.matches("(MDR|PC|MBR|MBRU|SP|LV|CPP|TOS|OPC)ORH"))
+			return F1 + ENA + ENB;
+		//0
+		if (s.matches("0"))
+			return F1;
+		//1
+		if (s.matches("1"))
+			return F0 + F1 + INC;
+		//-1
+		if (s.matches("(-|−)1"))
+			return F0 + F1 + INVA;
+
+		System.out.println("error in calculation ALU value");
+		return 0;
 	}
 
 	public static long calcNextAddress(String addressPart) {
@@ -50,36 +119,62 @@ public class Main {
 		else hexString = addressPart;
 
 		String binaryString = hexToBin(hexString).concat("000000000000000000000000000");
-		return Long.parseLong(binaryString,2);
+		return Long.parseLong(binaryString, 2);
 	}
 
 	public static long calcMemActions(String memPart) {
-		return 0;
+		long returnValue = 0;
+		switch (memPart) {
+			case "fetch":
+				returnValue += Math.pow(2, 4);
+				break;
+			case "rd":
+			case "read":
+				returnValue += Math.pow(2, 5);
+				break;
+			case "wr":
+			case "write":
+				returnValue += Math.pow(2, 6);
+				break;
+			default:
+				returnValue += 0;
+		}
+		return returnValue;
 	}
 
-	public static long calcCBusValue(String [] parts, int stopAt){
+	public static long calcCBusValue(String[] parts, int stopAt) {
 		long returnValue = 0;
-		for (int i = 0; i < stopAt; i++){
+		for (int i = 0; i < stopAt; i++) {
 			switch (parts[i]) {
-				case "H": returnValue += Math.pow(2,15);
-				break;
-				case "OPC": returnValue += Math.pow(2,14);
-				break;
-				case "TOS": returnValue += Math.pow(2,13);
-				break;
-				case "CPP": returnValue += Math.pow(2,12);
-				break;
-				case "LV": returnValue += Math.pow(2,11);
-				break;
-				case "SP": returnValue += Math.pow(2,10);
-				break;
-				case "PC": returnValue += Math.pow(2,9);
-				break;
-				case "MDR": returnValue += Math.pow(2,8);
-				break;
-				case "MAR": returnValue += Math.pow(2,7);
-				break;
-				default: returnValue += 0;
+				case "H":
+					returnValue += Math.pow(2, 15);
+					break;
+				case "OPC":
+					returnValue += Math.pow(2, 14);
+					break;
+				case "TOS":
+					returnValue += Math.pow(2, 13);
+					break;
+				case "CPP":
+					returnValue += Math.pow(2, 12);
+					break;
+				case "LV":
+					returnValue += Math.pow(2, 11);
+					break;
+				case "SP":
+					returnValue += Math.pow(2, 10);
+					break;
+				case "PC":
+					returnValue += Math.pow(2, 9);
+					break;
+				case "MDR":
+					returnValue += Math.pow(2, 8);
+					break;
+				case "MAR":
+					returnValue += Math.pow(2, 7);
+					break;
+				default:
+					returnValue += 0;
 			}
 		}
 		return returnValue;
